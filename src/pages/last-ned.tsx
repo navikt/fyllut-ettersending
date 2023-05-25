bd06b68146f529492317fb2aba6f26996cf44f0c
Thomas Tykesson <thomas@tykesson.com>
2023-05-25 09:13:25 +0200

    Endrer tekster på last-ned -siden

--- src/pages/last-ned.tsx
+++ src/pages/last-ned.tsx
@@ -11,14 +11,29 @@ interface Props {
   url: string;
 }
 
-const attachmentsHeader = {
-  single: "2. Dette dokumentet må du skaffe selv",
-  plural: "2. Disse dokumentene må du skaffe selv",
+const texts = {
+  title: {
+    formDocumentation: "Ettersende dokumentasjon i posten",
+    otherDocumentation: "Sende inn dokumentasjon",
+  },
+  attachmentsHeader: {
+    single: "2. Finn frem følgende dokument",
+    plural: "2. Finn frem følgende dokumenter",
+  },
+  lastSectionBody: {
+    formDocumentation:
+      "Legg førstesidearket fra punkt 1 på toppen av dokumentene. Send det hele til adressen på førstesidearket.",
+    otherDocumentation:
+      "Legg førstesidearket fra punkt 1 på toppen av dokumentene du skal sende. Send det hele til adressen på førstesidearket.",
+  },
 };
 
 const LastNed: NextPage<Props> = () => {
   const { formData } = useFormState();
   const [loading, setLoading] = useState<boolean>(false);
+
+  const isOtherDocumentation = !formData.formId;
+  const submissionType = isOtherDocumentation ? "otherDocumentation" : "formDocumentation";
 
   const download = async () => {
     setLoading(true);
@@ -30,7 +45,7 @@ const LastNed: NextPage<Props> = () => {
   };
 
   return (
-    <Layout title="Ettersende dokumentasjon i posten">
+    <Layout title={texts.title[submissionType]}>
       <Section>
         <Heading level="2" size="medium" spacing>
           1. Last ned førsteside til saken din
@@ -49,7 +64,7 @@ const LastNed: NextPage<Props> = () => {
       {formData.attachments && (
         <Section>
           <Heading level="2" size="medium" spacing>
-            {formData.attachments.length > 1 ? attachmentsHeader.plural : attachmentsHeader.single}
+            {formData.attachments.length > 1 ? texts.attachmentsHeader.plural : texts.attachmentsHeader.single}
           </Heading>
           <ul>
             {formData.attachments.map((attachment) => (
@@ -66,10 +81,7 @@ const LastNed: NextPage<Props> = () => {
           {formData.attachments ? "3" : "2"}. Send dokumentene til NAV i posten
         </Heading>
 
-        <BodyShort spacing>
-          Samle alle vedleggene fra punkt 2 og 3. Legg førstesidearket fra punkt 1 øverst. Send det hele til adressen på
-          førstesidearket.
-        </BodyShort>
+        <BodyShort spacing>{texts.lastSectionBody[submissionType]}</BodyShort>
       </div>
     </Layout>
   );
