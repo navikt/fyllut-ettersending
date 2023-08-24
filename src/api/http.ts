52860b8f582041f15fd5986ca6246dabd297b5b7
Morten Stulen <morten.elias.stulen@nav.no>
2023-08-24 15:25:42 +0200

    Fjern ubrukt kode

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,8 +1,3 @@
-const SubmissionMethodType = {
-  DIGITAL: "digital",
-  PAPER: "paper",
-} as const;
-
 const MimeType = {
   JSON: "application/json",
   TEXT: "text/plain",
@@ -15,27 +10,16 @@ type HttpHeaders = {
   "Content-Type"?: MimeType;
   Accept?: MimeType;
   Authorization?: string;
-  sub: string;
   [header: string]: string | string[] | undefined;
 };
 
-class UnauthenticatedError extends Error {}
-
 const getDefaultHeaders = (headers?: HttpHeaders) => {
-  const submissionMethod = headers?.["sub"]?.toUpperCase();
-
   const defaultHeaders = {
     "Content-Type": MimeType.JSON,
     Accept: MimeType.JSON,
   };
 
-  if (submissionMethod && submissionMethod in SubmissionMethodType) {
-    return {
-      ...defaultHeaders,
-      "Ettersendelse-Submission-Method": SubmissionMethodType[submissionMethod as keyof typeof SubmissionMethodType],
-    };
-  }
-  return { ...defaultHeaders };
+  return { ...defaultHeaders, ...headers };
 };
 
 const get = async (url: string, headers?: HttpHeaders) => {
@@ -59,10 +43,6 @@ const post = async (url: string, body: BodyInit, headers?: HttpHeaders) => {
 
 const handleResponse = (response: Response, url: string) => {
   if (!response.ok) {
-    if (response.status === 401) {
-      throw new UnauthenticatedError(response.statusText);
-    }
-
     throw new Error(`${response.status} ${response.statusText} (${url})`);
   }
 
--- src/api/loginRedirect.ts
+++ src/api/loginRedirect.ts
@@ -9,7 +9,6 @@ const isDevelopment = () => {
 };
 
 const getIdPortenToken = async (context: GetServerSidePropsContext) => {
-  // If query param sub=digital, the user will be logged in and redirected based on existing ettersendelse applications
   const sub = context.query?.sub;
   const authHeader = context.req.headers.authorization;
 
@@ -37,9 +36,6 @@ const fetchEttersendinger = async (idportenToken: string, id: string) => {
       process.env.INNSENDING_API_AUDIENCE ?? "dev-gcp:team-soknad:innsending-api"
     );
 
-    console.log("tokenXToken", tokenxToken);
-
-    logger.info(`Fetching ettersendinger for ${id}`);
     const response = await fetch(
       `${process.env.INNSENDING_API_URL}/frontend/v1/skjema/${id}/soknader?soknadstyper=ettersendelse`,
       { headers: { Authorization: `Bearer ${tokenxToken}` } }
