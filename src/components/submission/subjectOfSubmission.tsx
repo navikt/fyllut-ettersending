c71513db83e052627e03527994e4a08a8a2a8078
Morten Stulen <morten.elias.stulen@nav.no>
2023-08-29 11:06:27 +0200

    Add combobox for subject

--- src/components/submission/subjectOfSubmission.tsx
+++ src/components/submission/subjectOfSubmission.tsx
@@ -1,26 +1,77 @@
-import { Select, TextField } from "@navikt/ds-react";
+import { TextField } from "@navikt/ds-react";
 import { KeyValue } from "../../data/domain";
 import Section from "../section/section";
 import { useFormState } from "../../data/appState";
-import {useCallback, useEffect} from "react";
+import { useEffect, useMemo } from "react";
+import { UNSAFE_Combobox } from "@navikt/ds-react";
 
 interface Props {
   archiveSubjects: KeyValue;
   subject?: string;
 }
 
+type KeyValueMap = {
+  [key: string]: string;
+};
+
+const findKeyByValue = (obj: KeyValueMap, value: string): string | undefined => {
+  return Object.keys(obj).find((key) => obj[key].toLowerCase() === value.toLowerCase());
+};
+
+// If subject is in query param and is valid -> update formData and don't render combobox
+// If subject is in query param and is invalid -> render combobox
+// If subject is not in query param -> render combobox
 const SubjectOfSubmission = ({ archiveSubjects, subject }: Props) => {
   const { formData, updateFormData, errors } = useFormState();
 
-  const updateSubjectInFormData = useCallback((subject: string) => {
-    updateFormData({subjectOfSubmission: subject, titleOfSubmission: archiveSubjects[subject]});
-  }, [archiveSubjects, updateFormData]);
+  const options = useMemo(() => {
+    return Object.values(archiveSubjects).sort((a, b) => (a > b ? 1 : -1));
+  }, [archiveSubjects]);
 
+  // Update formdata from query param
   useEffect(() => {
-    if (subject && Object.keys(archiveSubjects).length > 0 && subject !== formData.subjectOfSubmission) {
-      updateSubjectInFormData(subject);
+    const subjectExistsAndIsValid = !!subject && !!Object.values(archiveSubjects).length && !!archiveSubjects[subject];
+
+    if (subjectExistsAndIsValid && !formData.subjectOfSubmission) {
+      updateFormData({
+        subjectOfSubmission: subject,
+        titleOfSubmission: archiveSubjects[subject],
+      });
     }
-  }, [archiveSubjects, formData.subjectOfSubmission, subject, updateSubjectInFormData])
+  }, [archiveSubjects, formData.subjectOfSubmission, subject, updateFormData]);
+
+  const renderCombobox = () => {
+    const subjectExistsAndIsInvalid = !!subject && !!Object.values(archiveSubjects).length && !archiveSubjects[subject];
+
+    if (!subject || subjectExistsAndIsInvalid) {
+      return combobox();
+    }
+
+    return null;
+  };
+
+  const combobox = () => {
+    return (
+      <UNSAFE_Combobox
+        label="Velg tema for innsendingen"
+        options={options}
+        size="medium"
+        name="subjectOfSubmission"
+        error={errors.subjectOfSubmission}
+        isMultiSelect={false}
+        onToggleSelected={(option, isSelected) => {
+          if (isSelected) {
+            updateFormData({
+              subjectOfSubmission: findKeyByValue(archiveSubjects, option),
+              titleOfSubmission: option,
+            });
+          } else {
+            updateFormData({ subjectOfSubmission: undefined, titleOfSubmission: undefined });
+          }
+        }}
+      />
+    );
+  };
 
   return (
     <>
@@ -36,27 +87,8 @@ const SubjectOfSubmission = ({ archiveSubjects, subject }: Props) => {
         />
       </Section>
 
-      { !subject &&
-        <Section>
-          <Select
-            label="Velg tema for innsendingen"
-            size="medium"
-            name="subjectOfSubmission"
-            value={formData.subjectOfSubmission}
-            error={errors.subjectOfSubmission}
-            onChange={(evt) => updateSubjectInFormData(evt.target.value)}
-          >
-            <option></option>
-            {Object.keys(archiveSubjects)
-              .sort((a, b) => (a > b ? 1 : -1))
-              .map((subject, index) => (
-                <option key={index} value={subject}>
-                  {archiveSubjects[subject]}
-                </option>
-              ))}
-          </Select>
-        </Section>
-      }
+      <Section>{renderCombobox()}</Section>
+      <button onClick={() => console.log("HALLA", formData, errors)}>clickme</button>
     </>
   );
 };
