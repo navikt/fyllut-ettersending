c74aa15c85b8af4a82dd68aec1b59cb181ed62f9
Morten Stulen <morten.elias.stulen@nav.no>
2023-09-28 12:01:54 +0200

    Refactor formdetail

--- src/pages/detaljer/[id]/digital/index.tsx
+++ src/pages/detaljer/[id]/digital/index.tsx
@@ -1,35 +1,12 @@
 import "@navikt/ds-css";
-import { Alert, Heading, Ingress } from "@navikt/ds-react";
 import type { NextPage } from "next";
-import { EttersendelseApplication, Form, NavUnit, SubmissionType, UnauthenticatedError } from "../../../../data/domain";
-import ChooseAttachments from "../../../../components/attachment/chooseAttachments";
-import ButtonGroup from "../../../../components/button/buttonGroup";
-import ChooseUser from "../../../../components/submission/chooseUser";
-import { useFormState } from "../../../../data/appState";
-import { useCallback, useEffect, useState } from "react";
-import Section from "../../../../components/section/section";
-import Layout from "../../../../components/layout/layout";
-import { useRouter } from "next/router";
+import { EttersendelseApplication, Form, UnauthenticatedError } from "../../../../data/domain";
 import { GetServerSidePropsContext } from "next/types";
-import { fetchNavUnits } from "../../../../api/apiClient";
-import { Paths } from "../../../../data/text";
-import ChooseSubmissionType from "../../../../components/submission/chooseSubmissionType";
-import {
-  createSubmissionUrl,
-  getDefaultSubmissionType,
-  areBothSubmissionTypesAllowed,
-  isSubmissionTypePaper,
-  isSubmissionAllowed,
-  isSubmissionParamSet,
-} from "../../../../utils/submissionUtil";
-import { ButtonType } from "../../../../components/button/buttonGroupElement";
-import { ArrowLeftIcon, ArrowRightIcon } from "@navikt/aksel-icons";
 import { getIdPortenToken } from "src/api/loginRedirect";
 import { getEttersendinger, getForm } from "src/api/apiService";
 import { ServerResponse } from "http";
-import { useReffererPage } from "src/hooks/useReferrerPage";
 import { getServerSideTranslations } from "../../../../utils/i18nUtil";
-import { useTranslation } from "next-i18next";
+import FormDetail from "src/components/page/FormDetail";
 
 interface Props {
   form: Form;
@@ -37,119 +14,9 @@ interface Props {
   existingEttersendinger: EttersendelseApplication[];
 }
 
-const Detaljer: NextPage<Props> = (props) => {
-  const router = useRouter();
-  const { form, id } = props;
-  const { formData, resetFormData } = useFormState();
-  const [navUnits, setNavUnits] = useState<NavUnit[]>([]);
-  const referrerPage = useReffererPage();
-  const { t } = useTranslation("detaljer");
-  const { t: tCommon } = useTranslation("common");
-
-  const fetchData = useCallback(async () => {
-    setNavUnits(await fetchNavUnits());
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
-
-  const getNavUnitsConnectedToForm = (deviceTypes: string[] | undefined) => {
-    const navUnitsConnectedToForm: NavUnit[] = navUnits.filter((navUnit) => deviceTypes?.includes(navUnit.type));
-    return navUnitsConnectedToForm && !!Object.keys(navUnitsConnectedToForm).length
-      ? navUnitsConnectedToForm
-      : navUnits;
-  };
-
-  useEffect(() => {
-    if (id !== formData.formId) {
-      resetFormData({
-        formNumber: form.properties.formNumber,
-        title: form.title,
-        subjectOfSubmission: form.properties.subjectOfSubmission,
-        submissionType: getDefaultSubmissionType(form, router),
-        formId: id,
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id, formData]);
-
-  // If the page is not yet generated, this will be displayed
-  // initially until getStaticProps() finishes running
-  if (router.isFallback) {
-    return <div>{t("loading-text")}</div>;
-  }
-
-  const downloadButton: ButtonType = {
-    text: tCommon("button.next"),
-    path: Paths.downloadPage,
-    validateForm: true,
-    icon: <ArrowRightIcon aria-hidden />,
-    iconPosition: "right",
-  };
-
-  const submitButton: ButtonType = {
-    text: tCommon("button.next"),
-    external: true,
-    path: createSubmissionUrl(form, formData),
-    validateForm: true,
-    icon: <ArrowRightIcon aria-hidden />,
-    iconPosition: "right",
-  };
-
-  const previousButton: ButtonType = {
-    text: tCommon("button.previous"),
-    variant: "secondary",
-    icon: <ArrowLeftIcon aria-hidden />,
-    path: referrerPage,
-    external: true,
-  };
-
-  return (
-    <Layout title={t("title")} backUrl={referrerPage}>
-      <Section>
-        <Heading size="large" level="2">
-          {form.title}
-        </Heading>
-        <Ingress>{form.properties.formNumber}</Ingress>
-      </Section>
-
-      {isSubmissionAllowed(form) ? (
-        <>
-          {areBothSubmissionTypesAllowed(form) && !isSubmissionParamSet(router) && <ChooseSubmissionType />}
-
-          <ChooseAttachments form={form} />
-
-          {isSubmissionTypePaper(formData) && (
-            <ChooseUser
-              navUnits={getNavUnitsConnectedToForm(form.properties.navUnitTypes)}
-              shouldRenderNavUnits={form.properties.navUnitMustBeSelected}
-            />
-          )}
-
-          <ButtonGroup
-            buttons={[
-              formData.submissionType === SubmissionType.digital ? submitButton : downloadButton,
-              ...(referrerPage ? [previousButton] : []),
-            ]}
-          />
-          <ButtonGroup
-            center={!!referrerPage}
-            buttons={[
-              {
-                text: tCommon("button.cancel"),
-                path: Paths.base,
-                variant: "tertiary",
-              },
-            ]}
-          />
-        </>
-      ) : (
-        <Alert variant="info">{t("no-attachments-alert")}</Alert>
-      )}
-    </Layout>
-  );
+// For digital submissions we need to log in to see if there are existing "ettersendinger". This cannot be done statically.
+const DigitalDetaljer: NextPage<Props> = (props) => {
+  return <FormDetail {...props}></FormDetail>;
 };
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
@@ -213,4 +80,4 @@ const redirectToLogin = (context: GetServerSidePropsContext) => {
   };
 };
 
-export default Detaljer;
+export default DigitalDetaljer;
--- src/pages/detaljer/[id]/index.tsx
+++ src/pages/detaljer/[id]/index.tsx
@@ -1,31 +1,9 @@
 import "@navikt/ds-css";
-import { Alert, Heading, Ingress } from "@navikt/ds-react";
 import type { GetStaticProps, NextPage } from "next";
-import { EttersendelseApplication, Form, NavUnit, SubmissionType } from "../../../data/domain";
-import ChooseAttachments from "../../../components/attachment/chooseAttachments";
-import ButtonGroup from "../../../components/button/buttonGroup";
-import ChooseUser from "../../../components/submission/chooseUser";
-import { useFormState } from "../../../data/appState";
-import { useEffect } from "react";
-import Section from "../../../components/section/section";
-import Layout from "../../../components/layout/layout";
-import { useRouter } from "next/router";
-import { Paths } from "../../../data/text";
-import ChooseSubmissionType from "../../../components/submission/chooseSubmissionType";
-import {
-  createSubmissionUrl,
-  getDefaultSubmissionType,
-  areBothSubmissionTypesAllowed,
-  isSubmissionTypePaper,
-  isSubmissionAllowed,
-  isSubmissionParamSet,
-} from "../../../utils/submissionUtil";
-import { ButtonType } from "../../../components/button/buttonGroupElement";
-import { ArrowLeftIcon, ArrowRightIcon } from "@navikt/aksel-icons";
+import { EttersendelseApplication, Form, NavUnit } from "../../../data/domain";
 import { getForm, getForms, getNavUnits } from "src/api/apiService";
-import { useReffererPage } from "src/hooks/useReferrerPage";
 import { getServerSideTranslations } from "../../../utils/i18nUtil";
-import { useTranslation } from "next-i18next";
+import FormDetail from "src/components/page/FormDetail";
 
 interface Props {
   form: Form;
@@ -35,108 +13,7 @@ interface Props {
 }
 
 const Detaljer: NextPage<Props> = (props) => {
-  const router = useRouter();
-  const { form, id } = props;
-  const { formData, resetFormData } = useFormState();
-  const referrerPage = useReffererPage();
-  const { t } = useTranslation("detaljer");
-  const { t: tCommon } = useTranslation("common");
-
-  const getNavUnitsConnectedToForm = (deviceTypes: string[] | undefined) => {
-    const navUnitsConnectedToForm: NavUnit[] = props.navUnits.filter((navUnit) => deviceTypes?.includes(navUnit.type));
-    return navUnitsConnectedToForm && !!Object.keys(navUnitsConnectedToForm).length
-      ? navUnitsConnectedToForm
-      : props.navUnits;
-  };
-
-  useEffect(() => {
-    if (id !== formData.formId) {
-      resetFormData({
-        formNumber: form.properties.formNumber,
-        title: form.title,
-        subjectOfSubmission: form.properties.subjectOfSubmission,
-        submissionType: getDefaultSubmissionType(form, router),
-        formId: id,
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id, formData]);
-
-  // If the page is not yet generated, this will be displayed
-  // initially until getStaticProps() finishes running
-  if (router.isFallback) {
-    return <div>{t("loading-text")}</div>;
-  }
-
-  const downloadButton: ButtonType = {
-    text: tCommon("button.next"),
-    path: Paths.downloadPage,
-    validateForm: true,
-    icon: <ArrowRightIcon aria-hidden />,
-    iconPosition: "right",
-  };
-
-  const submitButton: ButtonType = {
-    text: tCommon("button.next"),
-    external: true,
-    path: createSubmissionUrl(form, formData),
-    validateForm: true,
-    icon: <ArrowRightIcon aria-hidden />,
-    iconPosition: "right",
-  };
-
-  const previousButton: ButtonType = {
-    text: tCommon("button.previous"),
-    variant: "secondary",
-    icon: <ArrowLeftIcon aria-hidden />,
-    path: referrerPage,
-    external: true,
-  };
-
-  return (
-    <Layout title={t("title")} backUrl={referrerPage}>
-      <Section>
-        <Heading size="large" level="2">
-          {form.title}
-        </Heading>
-        <Ingress>{form.properties.formNumber}</Ingress>
-      </Section>
-
-      {isSubmissionAllowed(form) ? (
-        <>
-          {areBothSubmissionTypesAllowed(form) && !isSubmissionParamSet(router) && <ChooseSubmissionType />}
-
-          <ChooseAttachments form={form} />
-
-          {isSubmissionTypePaper(formData) && (
-            <ChooseUser
-              navUnits={getNavUnitsConnectedToForm(form.properties.navUnitTypes)}
-              shouldRenderNavUnits={form.properties.navUnitMustBeSelected}
-            />
-          )}
-
-          <ButtonGroup
-            buttons={[
-              formData.submissionType === SubmissionType.digital ? submitButton : downloadButton,
-              ...(referrerPage ? [previousButton] : []),
-            ]}
-          />
-          <ButtonGroup
-            center={!!referrerPage}
-            buttons={[
-              {
-                text: tCommon("button.cancel"),
-                path: Paths.base,
-                variant: "tertiary",
-              },
-            ]}
-          />
-        </>
-      ) : (
-        <Alert variant="info">{t("no-attachments-alert")}</Alert>
-      )}
-    </Layout>
-  );
+  return <FormDetail {...props}></FormDetail>;
 };
 
 export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
