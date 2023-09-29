0fdd03051b6c2093a3718dca081ce9c75d2f2c3f
Morten Stulen <morten.elias.stulen@nav.no>
2023-09-29 09:41:47 +0200

    Fix bug with default submissionType

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -29,7 +29,7 @@ const getForm = async (formPath: string, language: string = "nb"): Promise<Form 
     logger.error(`Failed to load form ${formPath}`, e);
   }
 
-  form.attachments.sort((a: Attachment, b: Attachment) => {
+  form?.attachments.sort((a: Attachment, b: Attachment) => {
     if (b.otherDocumentation) {
       return -1;
     } else if (a.otherDocumentation) {
@@ -42,7 +42,7 @@ const getForm = async (formPath: string, language: string = "nb"): Promise<Form 
     ...form,
     properties: {
       formNumber: form?.properties.skjemanummer ?? null,
-      submissionType: form?.properties.ettersending ?? form?.properties.innsending ?? "PAPIR_OG_DIGITAL",
+      submissionType: form?.properties.ettersending ?? "PAPIR_OG_DIGITAL",
       navUnitTypes: form?.properties.enhetstyper ?? [],
       subjectOfSubmission: form?.properties.tema ?? null,
       navUnitMustBeSelected: form?.properties.enhetMaVelgesVedPapirInnsending ?? null,
@@ -103,13 +103,13 @@ const getEttersendinger = async (idportenToken: string, id: string) => {
     if (!isLocalDevelopment()) {
       tokenxToken = (await getTokenxToken(
         idportenToken,
-        process.env.INNSENDING_API_AUDIENCE ?? "dev-gcp:team-soknad:innsending-api"
+        process.env.INNSENDING_API_AUDIENCE ?? "dev-gcp:team-soknad:innsending-api",
       )) as string;
     }
 
     const response = await fetch(
       `${process.env.INNSENDING_API_URL}/frontend/v1/skjema/${id}/soknader?soknadstyper=ettersendelse`,
-      { headers: { Authorization: `Bearer ${tokenxToken}` } }
+      { headers: { Authorization: `Bearer ${tokenxToken}` } },
     );
 
     return await response.json();
