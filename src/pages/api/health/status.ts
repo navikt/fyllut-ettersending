34f6ef31f0ec3bb6f57df9fce57b5772ea6b5d7e
Morten Stulen <morten.elias.stulen@nav.no>
2023-10-16 15:41:46 +0200

    Change name

--- src/pages/api/health/status.ts
+++ src/pages/api/health/status.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-interface HealthStatus {
+interface ApplicationStatus {
   status: 'OK' | 'ISSUE' | 'DOWN';
   description?: string;
   logLink?: string;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<HealthStatus>) {
-  const response: HealthStatus = { status: 'OK', description: 'OK' };
+export default function handler(req: NextApiRequest, res: NextApiResponse<ApplicationStatus>) {
+  const response: ApplicationStatus = { status: 'OK', description: 'OK' };
   res.status(200).json(response);
 }
