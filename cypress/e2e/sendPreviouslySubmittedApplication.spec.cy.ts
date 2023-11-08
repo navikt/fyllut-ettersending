e14964e62e4cb133cf291130d23b40b2fd8b64e3
Thomas Tykesson <thomas@tykesson.com>
2023-11-08 15:28:05 +0100

    Tests for submissionType -page

--- cypress/e2e/sendPreviouslySubmittedApplication.spec.cy.ts
+++ cypress/e2e/sendPreviouslySubmittedApplication.spec.cy.ts
@@ -1,20 +1,10 @@
 import { TestButtonText } from './testUtils';
 
 describe.only('sendPreviouslySubmittedApplication', () => {
-  beforeEach(() => {
-    cy.intercept('GET', `${Cypress.config('baseUrl')}/api/forms`).as('getForms');
-    cy.visit('/ettersendelse');
-    cy.wait('@getForms');
-  });
-  after(() => {
-    cy.mocksRestoreRouteVariants();
-  });
-
   it('fill out and send documentation by mail', () => {
-    cy.get('[name="search"]').click();
-    cy.get('[name="search"]').type('førerhund');
-    cy.get('[data-cy="searchResults"]').findAllByRole('link').contains('papir').click(); // Paper
-    cy.url().should('include', '/detaljer');
+    cy.visit('/detaljer/form2');
+    cy.get('a').contains('Send i posten').click();
+    cy.url().should('include', '?sub=paper');
     cy.get('[type="checkbox"]').first().check();
     cy.get('[type="radio"]').check('hasSocialNumber');
     cy.get('[name="socialSecurityNo"]').click();
@@ -30,15 +20,26 @@ describe.only('sendPreviouslySubmittedApplication', () => {
   });
 
   it('fill out and send documentation digitally', () => {
-    cy.get('[name="search"]').click();
-    cy.get('[name="search"]').type('førerhund');
-    cy.get('[data-cy="searchResults"]').findAllByRole('link').contains('digital').click(); // Digital
-    cy.url().should('include', '/detaljer');
+    cy.visit('/detaljer/form2');
+    cy.get('a').contains('Send digitalt').click();
+    cy.url().should('include', '?sub=digital');
     cy.findByRole('checkbox', { name: 'Legeerklæring om alminnelig helsetilstand' }).check();
     cy.findByRole('checkbox', { name: 'Annen dokumentasjon' }).check();
     cy.get('button').contains(TestButtonText.next).click();
     cy.url().should('include', '/sendinn/opprettSoknadResource?erEttersendelse=true');
     cy.url({ decode: true }).should('include', 'skjemanummer=NAV 10-07.50');
     cy.url().should('include', 'vedleggsIder=N6,L9');
+  });
+
+  it('automatically navigate to ?sub=paper, fill out + submit', () => {
+    cy.visit('/detaljer/form3');
+    cy.url().should('include', '?sub=paper');
+    cy.get('[type="checkbox"]').first().check();
+    cy.get('[name="otherDocumentationTitle"]').type('Application for parental leave');
+    cy.get('[type="radio"]').check('hasSocialNumber');
+    cy.get('[name="socialSecurityNo"]').click();
+    cy.get('[name="socialSecurityNo"]').type('28119135003');
+    cy.get('button').contains(TestButtonText.next).click();
+    cy.url().should('include', '/last-ned');
   });
 });
