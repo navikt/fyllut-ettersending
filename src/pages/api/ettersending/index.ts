4dfa0aec7e94e7e3d2d58d6e97ce3daebd994d6c
Morten Stulen <morten.elias.stulen@nav.no>
2024-01-08 13:38:50 +0100

    Update endpoint to send correct status

--- src/pages/api/ettersending/index.ts
+++ src/pages/api/ettersending/index.ts
@@ -1,16 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createEttersending } from 'src/api/apiService';
 import { getIdPortenToken } from 'src/api/loginRedirect';
-import { EttersendingRequestBody } from '../../../data/domain';
+import { EttersendelseApplication, EttersendingRequestBody } from '../../../data/domain';
+import logger from '../../../utils/logger';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const body: EttersendingRequestBody = req.body;
     const idportenToken = (await getIdPortenToken(req.headers.authorization)) as string;
 
-    // FIXME: Send correct status code
-    res.status(200);
-    res.send(await createEttersending(idportenToken, body));
+    const response = await createEttersending(idportenToken, body);
+
+    if (response.ok) {
+      const ettersending = (await response.json()) as EttersendelseApplication;
+      res.status(200);
+      res.send(ettersending);
+    } else {
+      const errorResponse = await response.json();
+      logger.error('Error creating ettersending', errorResponse);
+
+      res.status(response.status);
+      res.send(errorResponse);
+    }
   }
 };
 
