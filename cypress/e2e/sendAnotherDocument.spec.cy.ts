aa807c2b24020861df05325c337346480b808776
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2024-09-02 10:57:19 +0200

    løspost paper submission in one spec

--- cypress/e2e/sendAnotherDocument.spec.cy.ts
+++ cypress/e2e/sendAnotherDocument.spec.cy.ts
@@ -259,10 +259,39 @@ describe('sendAnotherDocument', () => {
     });
   });
 
-  describe('hard navigate to download-page', () => {
-    it('should redirect back to lospost', () => {
+  describe('Navigation', () => {
+    it('should redirect back to lospost on hard navigate to download-page', () => {
       cy.visit('/lospost/last-ned');
       cy.url().should('not.include', '/last-ned');
+    });
+
+    it('should navigate back from download-page and keep input data', () => {
+      cy.visit('/lospost/paper?tema=BIL');
+      cy.wait('@getArchiveSubjects');
+
+      cy.findByRole('textbox', { name: 'Hvilken dokumentasjon vil du sende til NAV?' })
+        .should('exist')
+        .type('Lisenskostnader');
+
+      cy.findByRole('group', { name: 'Hvem gjelder innsendingen for?' })
+        .should('exist')
+        .within(() => {
+          cy.findByRole('radio', { name: 'Jeg har fødselsnummer eller d-nummer' }).should('exist').click();
+        });
+
+      cy.findByRole('textbox', { name: 'Fødselsnummer / d-nummer' }).should('exist').type('28880948417');
+
+      // Click next to open page for cover sheet download
+      cy.get('button').contains(TestButtonText.next).click();
+      cy.findByRole('heading', { name: 'Send inn dokumentasjon' }).should('exist');
+      cy.findByRole('button', { name: 'Last ned førsteside' }).should('exist');
+
+      // Navigate to previous page
+      cy.findByRole('button', { name: 'Gå tilbake' }).should('exist').click();
+      cy.findByRole('textbox', { name: 'Hvilken dokumentasjon vil du sende til NAV?' })
+        .should('exist')
+        .should('contain.value', 'Lisenskostnader');
+      cy.findByRole('button', { name: 'Last ned førsteside' }).should('not.exist');
     });
   });
 });
