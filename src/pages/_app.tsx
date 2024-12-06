bad48b9e693ffdd50cede4479ed91d060fb02405
Magnus Røed Hestvik <mrh@computas.com>
2024-12-06 11:00:35 +0100

    google-site-verification got lost in merge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <FormDataProvider page={page} documentationTitlePrefix={documentationTitlePrefix}>
       <Head>
         <title>Dokument til NAV - nav.no</title>
+        <meta name="google-site-verification" content="TkSFJNNqsJLOyLN5yjG04ykxf61JTorXjhbEcYQkJd8" />
         {page !== 'lospost' && <meta name="robots" content="noindex" />}
         {page === 'lospost' && !!pageProps.tema && <link rel="canonical" href="/fyllut-ettersending/lospost" />}
       </Head>
