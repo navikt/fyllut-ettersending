503e068baa02cc8b205a121365184a5f602b6751
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2025-04-25 14:12:27 +0200

    Use Texas (nais) for token validation and exchange

--- src/auth/verifyIdPortenToken.ts
+++ src/auth/verifyIdPortenToken.ts
@@ -1,47 +1,41 @@
-import { FlattenedJWSInput, JWSHeaderParameters, JWTPayload, createRemoteJWKSet, jwtVerify } from 'jose';
-import { GetKeyFunction } from 'jose/dist/types/types';
-import { Client, Issuer } from 'openid-client';
+import logger from '../utils/logger';
 
-let _issuer: Issuer<Client>;
-let _remoteJWKSet: GetKeyFunction<JWSHeaderParameters, FlattenedJWSInput>;
-
-async function validerToken(token: string | Uint8Array) {
-  return jwtVerify(token, await jwks(), {
-    issuer: (await issuer()).metadata.issuer,
-  });
-}
-
-async function jwks() {
-  if (typeof _remoteJWKSet === 'undefined') {
-    const iss = await issuer();
-    _remoteJWKSet = createRemoteJWKSet(new URL(<string>iss.metadata.jwks_uri));
-  }
-
-  return _remoteJWKSet;
-}
-
-async function issuer() {
-  if (typeof _issuer === 'undefined') {
-    if (!process.env.IDPORTEN_WELL_KNOWN_URL) throw new Error('Miljøvariabelen "IDPORTEN_WELL_KNOWN_URL" må være satt');
-    _issuer = await Issuer.discover(process.env.IDPORTEN_WELL_KNOWN_URL);
-  }
-  return _issuer;
-}
-
-export const isExpired = (payload: JWTPayload) => {
-  if (payload.exp && payload.exp * 1000 <= Date.now()) {
-    return true;
-  }
-  return false;
+const texasConfig = {
+  introspection_endpoint: process.env.NAIS_TOKEN_INTROSPECTION_ENDPOINT!,
 };
 
-export async function verifyIdportenAccessToken(token: string) {
-  const verified = await validerToken(token);
-  if (isExpired(verified.payload)) {
+type ValidToken = {
+  active: true;
+  client_id: string;
+  acr: string;
+};
+
+type TexasIntrospectionResponse = { active: false } | ValidToken;
+
+export async function verifyIdportenAccessToken(token?: string) {
+  if (!token) {
+    throw new Error('IdPortenToken is undefined');
+  }
+  const response = await fetch(texasConfig.introspection_endpoint, {
+    body: JSON.stringify({
+      identity_provider: 'idporten',
+      token,
+    }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (!response.ok) {
+    logger.warn('Token introspection failed', { status: response.status, body: await response.text() });
+    throw new Error(`Token introspection failed: ${response.statusText}`);
+  }
+  const validatedToken: TexasIntrospectionResponse = await response.json();
+  if (!validatedToken.active) {
     throw new Error('IdPortenToken is expired');
   }
 
-  const { client_id, acr } = verified.payload;
+  const { client_id, acr } = validatedToken;
 
   if (client_id !== process.env.IDPORTEN_CLIENT_ID) {
     throw new Error('client_id matcher ikke min client ID');
@@ -50,5 +44,5 @@ export async function verifyIdportenAccessToken(token: string) {
   if (acr !== 'Level4' && acr !== 'idporten-loa-high') {
     throw new Error('For lavt sikkerhetsnivå - acr: ' + acr);
   }
-  return verified;
+  return validatedToken;
 }
