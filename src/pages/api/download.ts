d6e652d4d8819c3e7cf50f00824eecc94e0fd68b
Magnus Røed Hestvik <mrh@computas.com>
2025-05-05 12:20:40 +0200

    Handle error when downloading front page

--- src/pages/api/download.ts
+++ src/pages/api/download.ts
@@ -1,16 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { download } from '../../api/frontPageService';
-import { DownloadCoverPageRequestBody } from '../../data';
+import { DownloadCoverPageRequestBody, isHttpError } from '../../data';
 import { getFileName } from '../../utils/formDataUtil';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const lang = req.headers['accept-language'];
     const body: DownloadCoverPageRequestBody = req.body;
-    res.status(200);
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', `attachment; filename=${getFileName(body.formData)}`);
-    res.send(await download(body, lang));
+    try {
+      const result = await download(body, lang);
+      res.status(200);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', `attachment; filename=${getFileName(body.formData)}`);
+      res.send(result);
+    } catch (error) {
+      if (isHttpError(error)) {
+        res.status(error.status);
+        res.send({ message: error.message });
+      } else {
+        res.status(500);
+        res.send({ message: 'Internal server error' });
+      }
+    }
   }
 };
 
